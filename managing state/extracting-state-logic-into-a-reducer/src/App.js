487a0adb3637da24bc4f06867f66c68cd884a5dc
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2024-09-08 21:34:56 +0700

    what is reducer bro T_T

---
+++ managing state/extracting-state-logic-into-a-reducer/src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import AddTask from './AddTask';
+import TaskList from './TaskList';
+import { useReducer } from 'react';
+import tasksReducer from './tasksReducer';
+
+function TaskApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: 'added',
+      id: nextId++,
+      text: text,
+    })
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: 'changed',
+      task: task,
+    })
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: 'deleted',
+      id: taskId,
+    })
+  }
+    
+  return (
+    <>
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} onChangeTask={handleChangeTask}/>
+    </>
+  );
+}
+
+
+
+let nextId = 3;
+const initialTasks = [
+  {id: 0, text: 'Visit Kafka Museum', done: true},
+  {id: 1, text: 'Watch a puppet show', done: false},
+  {id: 2, text: 'Lennon Wall pic', done: false},
+]
+
+export default TaskApp;
